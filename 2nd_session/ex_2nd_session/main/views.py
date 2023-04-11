d92498482d7827f9f06b9a2d011fa17cf7b51226
IAMBH <xz7840@dgu.edu>
2023-04-11 15:05:22 +0900

    3주차 과제

--- 2nd_session/ex_2nd_session/main/views.py
+++ 2nd_session/ex_2nd_session/main/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def mainpage(request):
+    return render(request, 'main/mainpage.html')
+
+def interestpage(request):
+    return render(request, 'main/interestpage.html')
+
